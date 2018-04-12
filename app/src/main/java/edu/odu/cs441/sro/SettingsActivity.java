5bbdb85dddd2f2dcc2f6a6df2de28506b3f8dd0b
lswartz <lswartz@cs.odu.edu>
2018-04-12 16:25:09 -0400

    adding function stubs to SettingsActivty

--- app/src/main/java/edu/odu/cs441/sro/SettingsActivity.java
+++ app/src/main/java/edu/odu/cs441/sro/SettingsActivity.java
@@ -1,15 +1,72 @@
 package edu.odu.cs441.sro;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import edu.odu.cs441.sro.entity.metadata.Method;
+import edu.odu.cs441.sro.viewmodel.metadata.CategoryViewModel;
+import edu.odu.cs441.sro.viewmodel.metadata.LocationViewModel;
+import edu.odu.cs441.sro.viewmodel.metadata.MethodViewModel;
+import edu.odu.cs441.sro.viewmodel.record.ReceiptViewModel;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    ReceiptViewModel receiptViewModel;
+    LocationViewModel locationViewModel;
+    CategoryViewModel categoryViewModel;
+    MethodViewModel methodViewModel;
+
+    ArrayList<String> mySortByList ;
+    ArrayAdapter<String> mySortListAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        
+        receiptViewModel = ViewModelProviders.of(this).get(ReceiptViewModel.class);
+        locationViewModel = ViewModelProviders.of(this).get(LocationViewModel.class);
+        categoryViewModel = ViewModelProviders.of(this).get(CategoryViewModel.class);
+        methodViewModel = ViewModelProviders.of(this).get(MethodViewModel.class);
+
+        //add onClickListeners
+
+        //add ListView with onItemClickListeners
+
+        //add backButton
+
     }
+
+    public ArrayList<String> getMySortByList(ArrayList<String> myList) {
+        myList.add("PRICE_DESC");
+        myList.add("PRICE_ASCE");
+        myList.add("DATE_ASCE");
+        myList.add("DATE_DESC");
+        return myList;
+    }
+
+    public void addMethod()
+    {
+        Method newMethod = new Method("BitCoin");
+
+
+    }
+    public void addCategory()
+    {
+
+    }
+
+    public void addLocation()
+    {
+
+    }
+
+    public String changeSortOrder()
+    {
+     return "";
+    }
+
 }
