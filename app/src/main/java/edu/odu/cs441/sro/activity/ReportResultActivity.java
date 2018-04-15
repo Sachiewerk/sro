d0282ffdfe0222167b27189e6b0b48fe9170ae57
mpark <mpark@cs.odu.edu>
2018-04-15 19:51:40 -0400

    Started adding button event handler to ReportResultActivity.java

--- app/src/main/java/edu/odu/cs441/sro/activity/ReportResultActivity.java
+++ app/src/main/java/edu/odu/cs441/sro/activity/ReportResultActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import edu.odu.cs441.sro.R;
 
@@ -22,6 +24,21 @@ public class ReportResultActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.activity_report_result_textview);
 
         textView.setText(result);
-        Log.d("RESULT", result);
+
+        Button backButton = findViewById(R.id.activity_report_result_back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        Button emailButton = findViewById(R.id.activity_report_result_email_button);
+        emailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
     }
 }
