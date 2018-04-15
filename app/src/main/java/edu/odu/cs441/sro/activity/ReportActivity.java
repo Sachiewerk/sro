faa8042530196f364821e0b8080d497880e9e1e0
mpark <mpark@cs.odu.edu>
2018-04-15 13:17:29 -0400

    Created ReportActivity.java Fixed minor layout issues

---
+++ app/src/main/java/edu/odu/cs441/sro/activity/ReportActivity.java
@@ -0,0 +1,14 @@
+package edu.odu.cs441.sro.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import edu.odu.cs441.sro.R;
+
+public class ReportActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report);
+    }
+}
